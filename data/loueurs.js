753bf6b1e162321ca2fba20b7c9417e11ead64ca
Frederic Bonifas <fredericbonifas@gmail.com>
2014-06-19 14:40:19 +0200

    + draft interface

---
+++ data/loueurs.js
@@ -0,0 +1,11 @@
+var loueurs = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "id": 4, "nom": "Sun Ski" }, "geometry": { "type": "Point", "coordinates": [ 6.11906277889714, 45.005201321842627 ] } },
+{ "type": "Feature", "properties": { "id": 3, "nom": "Tyrol Sports" }, "geometry": { "type": "Point", "coordinates": [ 6.123692375601819, 45.006026581410154 ] } },
+{ "type": "Feature", "properties": { "id": 2, "nom": "Val d'Oisan" }, "geometry": { "type": "Point", "coordinates": [ 6.125330389690128, 45.016781608308897 ] } },
+{ "type": "Feature", "properties": { "id": 1, "nom": "Tyrol Sports" }, "geometry": { "type": "Point", "coordinates": [ 6.12438877680104, 45.009729700655015 ] } }
+]
+};
